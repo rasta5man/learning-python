58a9b3f2949c94ffbd7d715a1b38348e8e7fc40b
Tomas Radic <tomas.radic@gmail.com>
2018-09-02 15:22:29 +0200

    Testing git commits

--- Projekty/projekt-1/1_projekt_hotovy.py
+++ Projekty/projekt-1/1_projekt_hotovy.py
@@ -1,4 +1,4 @@
-'''
+''' 
 1. projekt - 5 bodov
 
 Napíšte program (skript v Pythone), ktorý bude vyrábať vizitky pre vašu firmu, napr. v takomto tvare
@@ -18,7 +18,7 @@ Vnútro vizitky mimo rámika má aspoň 5 riadkov a aspoň 30 znakov.
 Program najprv prečíta zo vstupu požadovaný počet vizitiek a šírku tlače (počet znakov na riadok) - vizitiek budete tlačiť maximálny počet krát
 vedľa seba. Posledný tlačený rad vizitiek tak môže byť kratší. Napr. pre šírku 120 a počet 10 vizitiek program vedľa seba vypíše 3 vizitky
 (šírka našej je 34 znakov), takýchto radov vizitiek bude 4, pričom v štvrtom rade bude už len jedna vizitka. Medzi vizitkami v riadku
-musí byť medzera a tiež medzi radmi vizitiek musí byť 1 riadok voľný. 
+musí byť medzera a tiež medzi radmi vizitiek musí byť 1 riadok voľný.
 '''
 # 1. projekt: vizitky pre firmu Jablka a Hrusky s.r.o.
 # autor: Rastislav Kovac
@@ -37,7 +37,7 @@ while pocetVizitiek > pocetVizitiekNaRiadok:
     print()
     for i in range(pocetVizitiekNaRiadok):
         print('|  (  #  )                      |', end = ' ')
-    print()    
+    print()
     for i in range(pocetVizitiekNaRiadok):
         print('|   \ @ /  Ing. Rastislav Kovac |', end = ' ')
     print()
@@ -60,7 +60,7 @@ for i in range(pocetVizitiek):
 print()
 for i in range(pocetVizitiek):
     print('|  (  #  )                      |', end = ' ')
-print()    
+print()
 for i in range(pocetVizitiek):
     print('|   \ @ /  Ing. Rastislav Kovac |', end = ' ')
 print()
