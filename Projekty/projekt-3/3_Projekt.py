0e6eb668275e6929eb5a033e61d0a5c132fc52ba
rasta5man <rastislavkovac@yahoo.com>
2018-09-07 12:53:03 +0200

    function (nameofday) added, from date to date calculation added

--- Projekty/projekt-3/3_Projekt.py
+++ Projekty/projekt-3/3_Projekt.py
@@ -59,17 +59,19 @@ Používajte len také konštrukcie jazyka Python, ktoré sme sa zatiaľ učili.
 
 
 
-
-
 # spusiaca funkcia 
 def kalendar(den, mesiac, rok, inyDen, inyMesiac, inyRok):
     dobryDatum(den, mesiac, rok, inyDen, inyMesiac, inyRok)
+    menoDna1(den, mesiac, rok, inyDen, inyMesiac, inyRok)
+    menoDna2(den, mesiac, rok, inyDen, inyMesiac, inyRok)
+    print('Pocet dni od 1.1.1901 po ', den,'.',mesiac,'.',rok,' = ', pocetDni1(den, mesiac, rok, inyDen, inyMesiac, inyRok))
+    print('Pocet dni od 1.1.1901 po ', inyDen,'.',inyMesiac,'.',inyRok,' = ', pocetDni2(den, mesiac, rok, inyDen, inyMesiac, inyRok))
+    pocet = abs(pocetDni1(den, mesiac, rok, inyDen, inyMesiac, inyRok) - pocetDni2(den, mesiac, rok, inyDen, inyMesiac, inyRok))
+    print('Pocet dni od', den,'.',mesiac,'.',rok,' po', inyDen,'.',inyMesiac,'.',inyRok,' = ', pocet)
     menoMesiaca(den, mesiac, rok, inyDen, inyMesiac, inyRok)
-    pocetDni(den, mesiac, rok, inyDen, inyMesiac, inyRok)
-    pocetDnivMesiaci(den, mesiac, rok, inyDen, inyMesiac, inyRok)
     vypisKalendara(den, mesiac, rok, inyDen, inyMesiac, inyRok)
-    menoDna(den, mesiac, rok, inyDen, inyMesiac, inyRok)
 
+    
 
 
 # funkcia skontroluje, ci je datum spravne zadany a ak nie, vypise o tom spravu    
@@ -81,7 +83,7 @@ def dobryDatum(den, mesiac, rok, inyDen, inyMesiac, inyRok):
             else:
                 print('zadali ste datum: ', den,'.',mesiac,'.',rok)
         if mesiac == 4 or mesiac==6 or mesiac ==9 or mesiac ==11:
-            if den not in range(1,30):
+            if den not in range(1,31):
                 print ('deň je zle zadaný dátum')
             else:
                 print('zadali ste datum: ', den,'.',mesiac,'.',rok)
@@ -101,7 +103,8 @@ def dobryDatum(den, mesiac, rok, inyDen, inyMesiac, inyRok):
         print('mesiac je zle zadaný dátum')
     if den not in range(1,32):
         print ('deň je zle zadaný dátum')
-
+        
+    
     #kontrola zadania pre iny rok
     if inyRok in range(1901,2101):
         if inyMesiac == 1 or inyMesiac==3 or inyMesiac ==5 or inyMesiac ==7 or inyMesiac ==8 or inyMesiac==10 or inyMesiac==12:
@@ -110,7 +113,7 @@ def dobryDatum(den, mesiac, rok, inyDen, inyMesiac, inyRok):
             else:
                 print('zadali ste datum: ', inyDen,'.',inyMesiac,'.',inyRok)
         if inyMesiac == 4 or inyMesiac==6 or inyMesiac ==9 or inyMesiac ==11:
-            if inyDen not in range(1,30):
+            if inyDen not in range(1,31):
                 print ('deň je zle zadaný dátum')
             else:
                 print('zadali ste datum: ', inyDen,'.',inyMesiac,'.',inyRok)
@@ -133,124 +136,86 @@ def dobryDatum(den, mesiac, rok, inyDen, inyMesiac, inyRok):
 
 
 
+
         
 
-# funkcia vypocita pocet dni od 1.1.1901 az po zadany datum      
-def pocetDni(den, mesiac, rok, inyDen, inyMesiac, inyRok):
-    februar = 28
+# funkcia vypocita pocet dni od 1.1.1901 az po prvy zadany datum      
+def pocetDni1(den, mesiac, rok, inyDen, inyMesiac, inyRok):
+    
     # nasledujuci for cyklus prida pocet dni podla poctu prestupnych rokov k zadanemu roku
-    pridaj = 0
-    for i in range(1904, 2101, 4):
+    pridaj1 = 0
+    for i in range(1904, 2104, 4):
             if rok >= i:
-                pridaj += 1
-    """vysledok:
-        1904 1908 1912 1916 1920 1924 1928 1932 1936 1940 1944 1948 1952 1956 1960
-        1964 1968 1972 1976 1980 1984 1988 1992 1996 2000 2004 2008 2012 2016 2020
-        2024 2028 2032 2036 2040 2044 2048 2052 2056 2060 2064 2068 2072 2076 2080 2084 2088 2092 2096 2100 
-    # prestupneRoky = 1904, 1908, 1912, 1916, 1920, 1924 ... = 1901 + (1*4-1), 1901 + (2*4-1), 1901+(3*4-1), 4*4-1
-    if rok-1901 == 4*x -1 
-    pridaj = rok - 1901
-    """
+                pridaj1 += 1
 
     if mesiac == 1:
-        pocetDni = 365*(rok - 1901) + den + pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni)
+        pocetDni = 365*(rok - 1901) + den + pridaj1
+    elif mesiac == 2:
+        pocetDni = 365*(rok - 1901) + 31 + den + pridaj1 
+    elif mesiac == 3:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + den+ pridaj1
+    elif mesiac == 4:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + den+ pridaj1
+    elif mesiac == 5:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + den+ pridaj1
+    elif mesiac == 6:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + den+ pridaj1
+    elif mesiac == 7:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + den+ pridaj1
+    elif mesiac == 8:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + den+ pridaj1
+    elif mesiac == 9:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + den+ pridaj1
+    elif mesiac == 10:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + den+ pridaj1
+    elif mesiac == 11:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + den+ pridaj1
+    elif mesiac == 12:
+        pocetDni = 365*(rok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + den+ pridaj1
+    
+    return pocetDni
 
-    if mesiac == 2:
-        pocetDni = 365*(rok - 1901) + 31 + den + pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni)
 
-    if mesiac == 3:
-        pocetDni = 365*(rok - 1901) + 31 + februar + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
+#funkcia vypocita pocet dni od 1.1.1901 az po druhy zadany datum
+def pocetDni2(den, mesiac, rok, inyDen, inyMesiac, inyRok):    
     
-    if mesiac == 4:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-    
-    if mesiac == 5:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-    
-    if mesiac == 6:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-    
-    if mesiac == 7:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-    
-    if mesiac == 8:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-    
-    if mesiac == 9:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-
-    if mesiac == 10:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + 30 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-
-    if mesiac == 11:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni) 
-    
-    if mesiac == 12:
-        pocetDni = 365*(rok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + den+ pridaj
-        print('pocet dni od 1.1.1901 do ', den,'.',mesiac,'.',rok, '=', pocetDni)
-
-    #vypocet poctu dni pre dalsi zadany datum
+    # priestupne roky
+    pridaj2 = 0
+    for i in range(1904, 2104, 4):
+            if inyRok >= i:
+                pridaj2 += 1
+        
     if inyMesiac == 1:
-        inyPocetDni = 365*(inyRok - 1901) + inyDen + pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni)
-
-    if inyMesiac == 2:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + inyDen + pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni)
-
-    if inyMesiac == 3:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 4:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 5:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 6:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 7:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 8:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 9:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-
-    if inyMesiac == 10:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + 30 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-
-    if inyMesiac == 11:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni) 
-    
-    if inyMesiac == 12:
-        inyPocetDni = 365*(inyRok - 1901) + 31 + februar + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + inyDen+ pridaj
-        print('pocet dni od 1.1.1901 do ', inyDen,'.',inyMesiac,'.',inyRok, '=', inyPocetDni)
+        inyPocetDni = 365*(inyRok - 1901) + inyDen + pridaj2
+    elif inyMesiac == 2:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + inyDen + pridaj2
+    elif inyMesiac == 3:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + inyDen+ pridaj2
+    elif inyMesiac == 4:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + inyDen+ pridaj2
+    elif inyMesiac == 5:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + inyDen+ pridaj2
+    elif inyMesiac == 6:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + inyDen+ pridaj2
+    elif inyMesiac == 7:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + inyDen+ pridaj2
+    elif inyMesiac == 8:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + inyDen+ pridaj2
+    elif inyMesiac == 9:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + inyDen+ pridaj2
+    elif inyMesiac == 10:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + inyDen+ pridaj2
+    elif inyMesiac == 11:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + inyDen+ pridaj2
+    elif inyMesiac == 12:
+        inyPocetDni = 365*(inyRok - 1901) + 31 + 28 + 31 + 30 + 31 + 30 + 31 + 31 + 30 + 31 + 30 + inyDen+ pridaj2
+           
+    return inyPocetDni
 
 
-            
+
+
+
 def pocetDnivMesiaci(den, mesiac, rok, inyDen, inyMesiac, inyRok):
     if mesiac == 1 or mesiac == 3 or mesiac==5 or mesiac ==7 or mesiac == 8 or mesiac == 10 or mesiac == 12:
         return 31
@@ -263,6 +228,7 @@ def pocetDnivMesiaci(den, mesiac, rok, inyDen, inyMesiac, inyRok):
 
 
 
+
 def menoMesiaca(den, mesiac, rok, inyDen, inyMesiac, inyRok):
     if mesiac == 1:
         print('kalendar pre januar', rok)
@@ -272,6 +238,8 @@ def menoMesiaca(den, mesiac, rok, inyDen, inyMesiac, inyRok):
         print('kalendar pre marec', rok)
     elif mesiac ==4:
         print('kalendar pre april', rok)
+    elif mesiac ==5:
+        print('kalendar pre maj', rok)
     elif mesiac ==6:
         print('kalendar pre jun', rok)
     elif mesiac ==7:
@@ -291,16 +259,48 @@ def menoMesiaca(den, mesiac, rok, inyDen, inyMesiac, inyRok):
 
 
 
-# nedorobene     
-def menoDna(den, mesiac, rok, inyDen, inyMesiac, inyRok):
 
-    menoDna1, menoDna2, menoDna3, menoDna4, menoDna5, menoDna6, menoDna7 = 'utorok',  'streda', 'stvrtok', 'piatok', 'sobota', 'nedela', 'pondelok'
-    if den == 1 and mesiac == 1 and rok == 1901:
-        for i in range(1, 32):
-            print( i, 'je ', menoDna1 )
-            menoDna1,menoDna2,menoDna3, menoDna4, menoDna5, menoDna6, menoDna7 = menoDna2, menoDna3, menoDna4, menoDna5, menoDna6, menoDna7, menoDna1
-         
-    
+# vypocita meno dna pre zadany datum - pondelok, utorok, streda, atd ...   
+def menoDna1(den, mesiac, rok, inyDen, inyMesiac, inyRok):
+    pocet1 = pocetDni1(den, mesiac, rok, inyDen, inyMesiac, inyRok)
+    if pocet1 in list(range(1, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = utorok ')
+    elif pocet1 in list(range(2, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = streda ')
+    elif pocet1 in list(range(3, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = stvrtok ')
+    elif pocet1 in list(range(4, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = piatok ')
+    elif pocet1 in list(range(5, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = sobota ')
+    elif pocet1 in list(range(6, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = nedela ')
+    elif pocet1 in list(range(7, 73051, 7)):
+        print(den,'.',mesiac,'.',rok,' = pondelok ')    
+
+
+
+
+# vypocita meno dna pre druhy zadany datum - pondelok, utorok, streda, atd ...  
+def menoDna2(den, mesiac, rok, inyDen, inyMesiac, inyRok):
+    pocet2 = pocetDni2(den, mesiac, rok, inyDen, inyMesiac, inyRok)
+    if pocet2 in list(range(1, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = utorok ')
+    elif pocet2 in list(range(2, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = streda ')
+    elif pocet2 in list(range(3, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = stvrtok ')
+    elif pocet2 in list(range(4, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = piatok ')
+    elif pocet2 in list(range(5, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = sobota ')
+    elif pocet2 in list(range(6, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = nedela ')
+    elif pocet2 in list(range(7, 73051, 7)):
+        print(inyDen,'.',inyMesiac,'.',inyRok,' = pondelok ')
+
+
+
         
 #nedorobene       
 def vypisKalendara(den, mesiac, rok, inyDen, inyMesiac, inyRok):
@@ -312,6 +312,23 @@ def vypisKalendara(den, mesiac, rok, inyDen, inyMesiac, inyRok):
     #    print('{:3}{:3}{:3}{:3}{:3}{:3}{:3}'.format(menoDna1,menoDna2,menoDna3, menoDna4, menoDna5, menoDna6, menoDna7))
     menoDna1, menoDna2, menoDna3, menoDna4, menoDna5, menoDna6, menoDna7 = 'utorok',  'streda', 'stvrtok', 'piatok', 'sobota', 'nedela', 'pondelok'
     if den == 1 and mesiac == 1 and rok == 1901:
-        for i in range(1, 32):
-            print( i, end=' ' )
-            menoDna1,menoDna2,menoDna3, menoDna4, menoDna5, menoDna6, menoDna7 = menoDna2, menoDna3, menoDna4, menoDna5, menoDna6, menoDna7, menoDna1
+        utorok = 1
+        streda = 2
+        stvrtok = 3
+        piatok = 4
+        sobota = 5
+        nedela = 6
+        pondelok = 7
+
+
+
+
+
+
+
+
+
+
+
+
+
